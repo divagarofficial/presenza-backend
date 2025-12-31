1b537b0bd382d4613a14379e922e0a101622e32f
DIVAGAR E <divagareofficial10@gmail.com>
2025-12-31 22:32:39 +0530

    Fix Neon SSL connection issue

--- app/database.py
+++ app/database.py
@@ -6,7 +6,13 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,     # ✅ checks connection before use
+    pool_recycle=300,       # ✅ recycle connections every 5 min
+)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
